0a25862bc3b63bd3b227db5a10b579a0416a9671
Priyatosh Anand <priyatoshanand92@gmail.com>
2023-08-10 21:29:41 +0530

    stage1 update

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -21,9 +21,20 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    
+    source_data_dir = config["source_data"]["data_dir"]
+    source_data_file = config["source_data"]["data_file"]
+    source_data_path = os.path.joim(source_data_dir, source_data_file)
 
+    split = params["preapare"]["split"]
+    seed = params["prepare"]["seed"]
+ 
+    random.seed(seed)
 
+    artifacts = config["artifacts"]
+    prepare_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["PREPARED_DATA"])
+    create_directories([prepare_data_dir_path])
+    
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
